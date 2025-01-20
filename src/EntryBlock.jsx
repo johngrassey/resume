c8594bf482e27122ac4cbc4ef05d2b85db1faaf8
John Grassey <johngrassey@gmail.com>
2025-01-19 20:55:13 -0500

    Connect General to EntryBlock

---
+++ src/EntryBlock.jsx
@@ -0,0 +1,12 @@
+import General from "./General";
+
+function EntryBlock() {
+  return (
+    <div className="entryblock">
+      <h1>Entry Block</h1>
+      <General />
+    </div>
+  );
+}
+
+export default EntryBlock;
